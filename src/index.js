a4ddcde6a199e22014b10837d750c9f5f7e817dd
rdroe <d@vidroe.com>
2021-06-27 12:39:08 -0500

    rid roebooks-centric stuff

--- src/index.js
+++
@@ -1,8 +0,0 @@
-var DebugType;
-(function (DebugType) {
-    DebugType["A"] = "a";
-    DebugType["B"] = "b";
-})(DebugType || (DebugType = {}));
-export var DEFAULT_DEBUG_TYPE = "a";
-export var debugChoices = ["a", "b"];
-//# sourceMappingURL=index.js.map
