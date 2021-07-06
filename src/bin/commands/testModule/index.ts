45227ed7f2a895cf61f2f774b84151a3601e1573
rdroe <d@vidroe.com>
2021-07-05 19:18:16 -0500

    move stem, leaf to setUp module

--- src/bin/commands/testModule/index.ts
+++ src/bin/commands/testModule/index.ts
@@ -1,36 +1,7 @@
 
-import { CommandModule, Options } from 'yargs'
-import { Action, AppOptions, AppArgv } from '../../../../index'
-
-export const stem = (nm: string, subs: CommandModule[], desc: string = nm) => {
-    const subOrSubs = subs[0] && typeof subs[0].builder === 'object' ? 'sub' : 'sub..'
-    return {
-        command: `${nm} [${subOrSubs}] [options]`,
-        describe: desc,
-        builder: (yargs) => {
-            subs.forEach((module: CommandModule) => {
-                yargs.command(module)
-            })
-            return yargs.demandCommand()
-        }
-    } as CommandModule
-}
-
-export const leaf = (nm: string, opts: AppOptions, action: Action, desc: string = nm): CommandModule => {
-    return {
-        command: `${nm} [options]`,
-        describe: desc,
-        builder: opts,
-        handler: (args) => {
-
-            const { a: a_, anarg: a2_ } = args
-            const str = a_ ?? a2_
-
-            if (typeof (str) !== 'string' && typeof (str) !== 'number') throw new Error(`string is required for option anarg`)
-            action({ anarg: str })
-        }
-    }
-}
+import { Options } from 'yargs'
+import { Action, AppOptions, AppArgv } from '../../appTypes'
+import { leaf, stem } from '../../setUp'
 
 const anarg: Options = {
     alias: 'a',
--- src/bin/setUp.ts
+++ src/bin/setUp.ts
@@ -1,7 +1,41 @@
-import { CommandModule } from 'yargs'
+import yargs, { CommandModule } from 'yargs'
+import { AppOptions, Action } from './appTypes'
 import matchCmd from './commands/match'
-const yargs = require('yargs')
+
 const match: CommandModule = matchCmd
+
+
+export const stem = (nm: string, subs: CommandModule[], desc: string = nm) => {
+    const subOrSubs = subs[0] && typeof subs[0].builder === 'object' ? 'sub' : 'sub..'
+    return {
+        command: `${nm} [${subOrSubs}] [options]`,
+        describe: desc,
+        builder: (yargs) => {
+            subs.forEach((module: CommandModule) => {
+                yargs.command(module)
+            })
+            return yargs.demandCommand()
+        }
+    } as CommandModule
+}
+
+export const leaf = (nm: string, opts: AppOptions, action: Action, desc: string = nm): CommandModule => {
+    return {
+        command: `${nm} [options]`,
+        describe: desc,
+        builder: opts,
+        handler: (args) => {
+
+            const { a: a_, anarg: a2_ } = args
+            const str = a_ ?? a2_
+
+            if (typeof (str) !== 'string' && typeof (str) !== 'number') throw new Error(`string is required for option anarg`)
+            action({ anarg: str })
+        }
+    }
+}
+
+
 export default (modules: CommandModule[]) => {
     yargs.usage("$0 command")
     const allModules: CommandModule[] = modules
