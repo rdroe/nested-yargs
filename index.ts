5ddd5dae054c03ec3b5116684011df05ce9050e7
rdroe <d@vidroe.com>
2021-08-01 21:12:27 -0500

    add hook and repl stuff

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import setUp, { repl as repl_ } from './src/setUp'
 import { get as apiGet, post as apiPost, SaveRequest as SR, QueryParams as QP } from './src/lib/api/call'
 import { AppOptions as AO, Action as A, AppArgv as AA } from './src/appTypes'
 
+import { object as qObj } from './src/queue'
+
 export default setUp
 export const repl = repl_
 export type SaveRequest = SR
@@ -12,4 +14,5 @@ export const get = apiGet
 export type AppOptions = AO
 export type Action = A
 export type AppArgv = AA
-
+export const object = qObj
+export const hook = qObj.resolver
--- src/setUp.ts
+++ src/setUp.ts
@@ -1,9 +1,56 @@
 import yargs, { CommandModule } from 'yargs'
 import matchCmd from './commands/match'
 import stringArgv from 'string-argv'
+import { add } from './queue'
+import readline from 'readline'
 const match: CommandModule = matchCmd
 
-export const repl = async (modules: CommandModule[], input: string = '') => {
+
+export default async (modules: CommandModule[], replStr: string | undefined = '') => {
+    yargs.usage("$0 command")
+    const allModules: CommandModule[] = modules
+    allModules.push(match)
+    const moduleProms = allModules.map(async (module: CommandModule) => yargs.command(module).argv)
+    await Promise.all(moduleProms)
+    yargs.demand(1, "You must provide a valid command")
+        .help("h")
+        .alias("h", "help").argv
+    return true
+}
+
+
+export const repl = (modules: CommandModule[]) => {
+    const rb = (input: string) => {
+        return new Promise((res) => {
+            add(res)
+            return repl_(modules, input)
+        })
+    }
+
+
+    const callRb = () => {
+        return new Promise((replRes) => {
+            const rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout
+            });
+
+            rl.question('rb > ', (input) => {
+                rb(input)
+                    .then(x => {
+                        rl.close()
+                        replRes(x)
+                        callRb()
+                    })
+            });
+        })
+    }
+
+    callRb()
+}
+
+
+async function repl_(modules: CommandModule[], input: string = '') {
     const simArgv = input === '' ? undefined : stringArgv(input)
 
     yargs(simArgv).usage("$0 command")
@@ -12,20 +59,6 @@ export const repl = async (modules: CommandModule[], input: string = '') => {
     const moduleProms = allModules.map(async (module: CommandModule) => yargs(simArgv).command(module).argv)
     await Promise.all(moduleProms)
     yargs(simArgv).demand(1, "You must provide a valid command")
-        .help("h")
-        .alias("h", "help").argv
-    return true
-}
-
-export default async (modules: CommandModule[], replStr: string | undefined = '') => {
-    if (replStr) { return repl(modules, replStr) }
-
-    yargs.usage("$0 command")
-    const allModules: CommandModule[] = modules
-    allModules.push(match)
-    const moduleProms = allModules.map(async (module: CommandModule) => yargs.command(module).argv)
-    await Promise.all(moduleProms)
-    yargs.demand(1, "You must provide a valid command")
         .help("h")
         .alias("h", "help").argv
     return true
