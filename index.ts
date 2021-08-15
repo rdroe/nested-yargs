ae12727abb956c7958c6d53fe0162461281e1bc1
rdroe <d@vidroe.com>
2021-08-15 16:47:12 -0500

    redo exports index

--- index.ts
+++ index.ts
@@ -1,17 +1,12 @@
 
-import setUp, { repl as repl_ } from './src/setUp'
-import { get as apiGet, post as apiPost, SaveRequest as SR, QueryParams as QP } from './src/lib/api/call'
-import { AppOptions as AO, Action as A, AppArgv as AA } from './src/appTypes'
-
+import setUp from './src/setUp'
 import hooks_ from './src/hooks'
 
+
 export default setUp
-export const repl = repl_
-export type SaveRequest = SR
-export type QueryParams = QP
-export const post = apiPost
-export const get = apiGet
-export type AppOptions = AO
-export type Action = A
-export type AppArgv = AA
 export const hooks = hooks_
+
+export { AppOptions, Action, AppArgv } from './src/appTypes'
+export { repl } from './src/setUp'
+export { get, post, QueryParams, SaveRequest } from './src/lib/api/call'
+
